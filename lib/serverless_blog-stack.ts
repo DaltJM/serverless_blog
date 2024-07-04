e8dc9bdf1263d8c16b1f646392d863f1dbd9b23a
DaltJM <56285459+DaltJM@users.noreply.github.com>
2024-07-04 23:04:16 +1000

    init userPool setup
    Setup a basic cognito userpool for username and password signup with enforced MFA

--- lib/serverless_blog-stack.ts
+++ lib/serverless_blog-stack.ts
@@ -1,16 +1,55 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Duration, aws_cognito } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export interface ServerlessBlogStackProps extends cdk.StackProps {}
 
 export class ServerlessBlogStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: ServerlessBlogStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // User Auth
+    // Cognito
+    const cognitouserPool = new aws_cognito.UserPool(this, "blogUserPool", {
+      accountRecovery: aws_cognito.AccountRecovery.EMAIL_ONLY,
+      mfa: aws_cognito.Mfa.REQUIRED,
+      mfaSecondFactor: {
+        otp: true,
+        sms: false,
+      },
+      email: aws_cognito.UserPoolEmail.withSES({ // Will need to setup an SES verifed email/identity and provide cognito permissions to use it 
+        fromEmail: "no-reply@placeholder.com",
+        fromName: "serverlessBlog",
+        sesVerifiedDomain: "",
+      }),
+      passwordPolicy: {
+        minLength: 12,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+        tempPasswordValidity: Duration.days(3),
+      },
+      signInAliases: { username: true, email: true },
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: "",
+        emailBody: "",
+        emailStyle: aws_cognito.VerificationEmailStyle.LINK
+      }
+    });
+    // Blog post management
+    // DynamoDB
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessBlogQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // API Gateway
+
+    // Lambda
+    // Frontend and hosting
+    // S3
+
+    // Cloudfront
+
+    // Monitoring and logging
+    // Cloudwatch
   }
 }
