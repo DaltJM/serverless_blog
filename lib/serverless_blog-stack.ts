b4f24b94216aceb74e330bafc27cc8e39a66e6bf
DaltJM <56285459+DaltJM@users.noreply.github.com>
2024-07-05 19:21:56 +1000

    Added comments and updated type in cognito const
    cognitouserPool should have been cognitoUserPool. Added comments for each prop used in Cognito so far.

--- lib/serverless_blog-stack.ts
+++ lib/serverless_blog-stack.ts
@@ -15,35 +15,56 @@ export class ServerlessBlogStack extends cdk.Stack {
     super(scope, id, props);
 
     // User Auth
-    // Cognito
-    const cognitouserPool = new aws_cognito.UserPool(this, "blogUserPool", {
+    // Cognito User Pool
+    const cognitoUserPool = new aws_cognito.UserPool(this, "blogUserPool", {
+      // Sets account recovery to email only with a link
       accountRecovery: aws_cognito.AccountRecovery.EMAIL_ONLY,
+      // Enforces the use of an MFW device
       mfa: aws_cognito.Mfa.REQUIRED,
+      // Enable the use of OTP devices and disables MFA via SMS
       mfaSecondFactor: {
         otp: true,
         sms: false,
       },
+      // Configure Cognito to send emails to users with SES
       email: aws_cognito.UserPoolEmail.withSES({ // Will need to setup an SES verifed email/identity and provide cognito permissions to use it 
+        // The from email used for sending emails
         fromEmail: props.cognitoFromEmail,
+        // The name that appears on the sent emails
         fromName: props.cognitoFromName,
+        // SES Verified custom domain to be used to verify the identity.
         sesVerifiedDomain: props.cognitoSesVerifiedDomain,
       }),
+      // Sets a password policy
       passwordPolicy: {
+        // Minimum length of 12 characters
         minLength: 12,
+        // Requires the use of at least 1 lowercase letter
         requireLowercase: true,
+        // Requires the use of at least 1 uppsercase letter
         requireUppercase: true,
+        // RFequires the use of at least 1 number
         requireDigits: true,
+        // Requires the use of at least 1 symbol
         requireSymbols: true,
+        // Sets the duration a temporary password is valid for when created by an Administrator
         tempPasswordValidity: Duration.days(3),
       },
+      // Allows users to signup/signin with email or username
       signInAliases: { username: true, email: true },
+      // Allow users to register their own accounts
       selfSignUpEnabled: true,
+      // Configure the email that is sent to a user when they sign themselves up
       userVerification: {
+        // Subject of the email
         emailSubject: "",
+        // Body of the email
         emailBody: "",
+        // Sends a link the user can follow to verify their account
         emailStyle: aws_cognito.VerificationEmailStyle.LINK
       }
     });
+    
     // Blog post management
     // DynamoDB
 
